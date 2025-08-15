a994ae51135f7c86dbc97cb5d4682aff9472d60c
Atharva Pardeshi <atharva.exe@gmail.com>
2025-08-15 21:53:22 +0530

    feat: scan two char(special?) lexemes

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -62,9 +62,31 @@ class Scanner {
       case '*':
         addToken(STAR);
         break;
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
       default:
         Lox.error(line, "Unexpected character.");
     }
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd())
+      return false;
+    if (source.charAt(current) != expected)
+      return false;
+
+    current++;
+    return true;
   }
 
   private boolean isAtEnd() {
