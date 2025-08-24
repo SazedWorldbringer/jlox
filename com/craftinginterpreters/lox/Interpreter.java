9ae9dce9036807df10116a64afd74f55a4e61b9d
Atharva Pardeshi <atharva.exe@gmail.com>
2025-08-24 13:07:24 +0530

    feat: evaluate binary operations
    recursively evaluate both left and right operands, then apply the
    operator

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,7 +1,6 @@
 package com.craftinginterpreters.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
-
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
@@ -30,6 +29,15 @@ class Interpreter implements Expr.Visitor<Object> {
     return true;
   }
 
+  private Boolean isEqual(Object a, Object b) {
+    if (a == null && b == null)
+      return true;
+    if (a == null)
+      return false;
+
+    return a.equals(b);
+  }
+
   @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
@@ -37,5 +45,45 @@ class Interpreter implements Expr.Visitor<Object> {
 
   private Object evaluate(Expr expr) {
     return expr.accept(this);
+  }
+
+  @Override
+  public Object visitBinaryExpr(Expr.Binary expr) {
+    Object left = evaluate(expr.left);
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case GREATER:
+        return (double) left > (double) right;
+      case GREATER_EQUAL:
+        return (double) left >= (double) right;
+      case LESS:
+        return (double) left < (double) right;
+      case LESS_EQUAL:
+        return (double) left <= (double) right;
+      case BANG_EQUAL:
+        return !isEqual(left, right);
+      case EQUAL_EQUAL:
+        return isEqual(left, right);
+      case MINUX:
+        return (double) left - (double) right;
+      case PLUS:
+        if (left instanceof Double && right instanceof Double) {
+          return (double) left + (double) right;
+        }
+
+        if (left instanceof String && right instanceof String) {
+          return (String) left + (String) right;
+        }
+
+        break;
+      case SLASH:
+        return (double) left / (double) right;
+      case STAR:
+        return (double) left * (double) right;
+    }
+
+    // unreachable
+    return null;
   }
 }
