51b0e7f5d3a2a344baccf725fc7a8db17e4ea2e7
Atharva Pardeshi <atharva.exe@gmail.com>
2025-08-24 12:21:53 +0530

    feat: handle grouping expr by recursively evaluating the inner expr

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -7,4 +7,12 @@ class Interpreter implements Expr.Visitor<Object> {
     return expr.value;
   }
 
+  @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
 }
