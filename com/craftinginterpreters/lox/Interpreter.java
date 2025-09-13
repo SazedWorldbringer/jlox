4f444433260d6307f695e954a7db3dd63b59f42b
Atharva Pardeshi <atharva.exe@gmail.com>
2025-09-13 06:35:04 +0530

    fix: evaluate if condition

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -179,7 +179,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitIfStmt(Stmt.If stmt) {
-    if (isTruthy(stmt.condition)) {
+    if (isTruthy(evaluate(stmt.condition))) {
       execute(stmt.thenBranch);
     } else if (stmt.elseBranch != null) {
       execute(stmt.elseBranch);
